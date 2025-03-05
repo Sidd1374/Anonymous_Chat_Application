6339f2a91fd8a05034a8595c3483ea56c8da804d
Aarju Pal <aarju.pal1234@gmail.com>
2025-03-05 18:18:31 +0530

    Added boilerplate code, removed sample file

---
+++ lib/components/component_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ComponentTest extends StatelessWidget {
+  const ComponentTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Component Test")),
+
+      body: Center(
+        child: Column(
+          children: [
+            // Put your components here
+            
+          ],
+        ),
+      ),
+    );
+  }
+} 
