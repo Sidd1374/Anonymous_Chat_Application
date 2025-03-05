5576418723928cbdce5d28301f686d0c37116e63
Aarju Pal <aarju.pal1234@gmail.com>
2025-03-05 23:55:57 +0530

    Creating User Cards components

---
+++ lib/components/userCard.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HorizontalUserCard extends StatelessWidget {
+  final VoidCallback? onPressed;
+  // Some functionality to get User Details
+  // Because we need to know if user is verified or not.
+
+  const HorizontalUserCard({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ElevatedButton(
+      onPressed: onPressed,
+
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        foregroundColor: Theme.of(context).textTheme.bodySmall?.color
+      ),
+
+
+      child: Row(
+
+
+        children: [
+
+          
+
+        ],
+      )
+    );
+
+  }
+}
