1374774bcc35adfcf2b6b726f0044a3c7021865c
Aarju Pal <aarju.pal1234@gmail.com>
2025-03-06 21:56:02 +0530

    Removed isEnabled func.; corrected hint and text colors

--- lib/components/input_field.dart
+++ lib/components/input_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  final bool isEnabled;
   final TextEditingController inputController; 
   final TextInputType keyboardType;
   final TextInputAction textInputAction; 
@@ -9,7 +8,6 @@ class InputField extends StatelessWidget {
 
   const InputField({
     super.key,
-    required this.isEnabled,
     required this.inputController,
     required this.keyboardType,
     required this.textInputAction,
@@ -25,18 +23,20 @@ class InputField extends StatelessWidget {
         keyboardType: keyboardType,
         textInputAction: textInputAction,
         minLines: 1, maxLines: 1,
-        enabled: isEnabled,
 
         // Styling
         cursorColor: Theme.of(context).textTheme.bodyMedium?.color,
 
+        style: TextStyle(
+          color: Theme.of(context).textTheme.bodySmall?.color,
+          fontWeight: FontWeight.w500
+        ),
+
         decoration: InputDecoration(
           hintText: hintText,
           hintStyle: TextStyle(
-            color: (isEnabled) 
-                  ? (Theme.of(context).textTheme.bodyMedium?.color?.withAlpha(150))
-                  : (Theme.of(context).textTheme.bodyMedium?.color?.withAlpha(100)),
-            fontWeight: FontWeight.w400,
+            color: Theme.of(context).textTheme.bodyMedium?.color?.withAlpha(127),
+            fontWeight: FontWeight.w500,
           ),
 
           contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 18),
@@ -44,12 +44,6 @@ class InputField extends StatelessWidget {
           filled: true,
           fillColor: Theme.of(context).colorScheme.secondary,
 
-          disabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.transparent,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(10))
-          ),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(
               color: Colors.transparent,
