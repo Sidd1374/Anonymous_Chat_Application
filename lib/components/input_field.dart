f6bf07309bcd3e6aa2f6b8c3315fcf8c35a2d0f4
Aarju Pal <aarju.pal1234@gmail.com>
2025-03-07 17:35:05 +0530

    Corrected Infinite vertical spacing caused by Expanded widget

--- lib/components/input_field.dart
+++ lib/components/input_field.dart
@@ -18,49 +18,47 @@ class InputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: TextField(
-        // Configuration
-        controller: inputController,
-        keyboardType: keyboardType,
-        textInputAction: textInputAction,
-        obscureText: isObscured,
-        minLines: 1, maxLines: 1,
-
-        // Styling
-        cursorColor: Theme.of(context).textTheme.bodyMedium?.color,
-
-        style: TextStyle(
-          color: Theme.of(context).textTheme.bodySmall?.color,
-          fontWeight: FontWeight.w500
+    return TextField(
+      // Configuration
+      controller: inputController,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      obscureText: isObscured,
+      minLines: 1, maxLines: 1,
+    
+      // Styling
+      cursorColor: Theme.of(context).textTheme.bodyMedium?.color,
+    
+      style: TextStyle(
+        color: Theme.of(context).textTheme.bodySmall?.color,
+        fontWeight: FontWeight.w500
+      ),
+    
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Theme.of(context).textTheme.bodyMedium?.color?.withAlpha(127),
+          fontWeight: FontWeight.w500,
         ),
-
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: TextStyle(
-            color: Theme.of(context).textTheme.bodyMedium?.color?.withAlpha(127),
-            fontWeight: FontWeight.w500,
+    
+        contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 18),
+    
+        filled: true,
+        fillColor: Theme.of(context).colorScheme.secondary,
+    
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
           ),
-
-          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 18),
-
-          filled: true,
-          fillColor: Theme.of(context).colorScheme.secondary,
-
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.transparent,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(10))
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.transparent,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(10))
-          ),
-           
+          borderRadius: BorderRadius.all(Radius.circular(10))
         ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
+         
       ),
     );
   }
